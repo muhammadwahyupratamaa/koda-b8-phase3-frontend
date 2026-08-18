97c16d0c5b116295d8559dce863a3364552f7f89
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-08-18 12:00:44 +0700

    feat: add login and register components with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 
+const router = createBrowserRouter([
+  // AUTH
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
 function App() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
